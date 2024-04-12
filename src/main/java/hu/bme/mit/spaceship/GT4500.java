972abe8c518512085802ae9bf65a4a8c0b191425
Marton Mack <mackmarton@edu.bme.hu>
2024-04-12 09:07:36 +0200

    Changes from branch-A

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,6 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
+        //CHANGES FROM BRANCH-A
         if (! primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
         }
